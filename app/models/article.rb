8b68a0905b8785054ec12156a1ecd66bcde1701f
Mutatak <kyl.mustafa@gmail.com>
2024-07-16 16:36:43 +0300

    chore: Update article model and views to use Action Text for content field

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
   include Visible
 
+  has_rich_text :content
+
   belongs_to :user
 
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 end
