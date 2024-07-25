02008b1d1427d19ac0468b7353e430e1fd56c77e
Mutatak <kyl.mustafa@gmail.com>
2024-07-25 15:49:11 +0300

    chore: Seed initial data for users, articles, and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,58 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# USER  ############################################
+# Create a user
+  @admin = User.create(
+    email: "bar@foo.com",
+    password: '123456',
+    password_confirmation: '123456',
+    confirmed_at: Time.now
+  )
+  if @admin.save
+    puts "User succesfully created"
+  end
+
+# Update a user
+  @admin.update(
+    password: '1234567',
+    password_confirmation: '1234567'
+  )
+
+# ARTICLE ############################################
+# Create articles
+  unless Article.exists?
+    @article = Article.new(
+      title: "Seed article",
+      content: "This is the content of the seed article",
+      user_id: 1,
+      status: 'public'
+    )
+
+    if @article.save
+      puts "Article succesfully created!"
+    end
+  else
+    puts "Article already exists!"
+  end
+
+# COMMENT ############################################
+# Create comments
+  unless Comment.exists?
+    @comment = Comment.create(
+      commentor: "fizbar",
+      body: "buzzbizz",
+      article_id: 1,
+      user_id: 1,
+      status: 'public'
+    )
+
+    if @comment.save
+      puts "Comment succesfully created!"
+    end
+  else
+    puts "Comment already exists!"
+  end
+
+puts 'Seeding is complete!'
