0277d04b505ed78b653b1616637d238e9f6850f2
Mutatak <kyl.mustafa@gmail.com>
2024-10-12 11:34:29 +0300

    Add puma.state and puma.stderr.log files

--- db/migrate/20240713074149_add_confirmable_to_user.rb
+++ db/migrate/20240713074149_add_confirmable_to_user.rb
@@ -1,11 +1,11 @@
 class AddConfirmableToUser < ActiveRecord::Migration[7.1]
   def change
     change_table :users do |t|
-     ## Confirmable
-     t.string   :confirmation_token
-     t.datetime :confirmed_at
-     t.datetime :confirmation_sent_at
-     t.string   :unconfirmed_email # Only if using reconfirmable
-     end
+      ## Confirmable
+      t.string   :confirmation_token unless column_exists?(:users, :confirmation_token)
+      t.datetime :confirmed_at unless column_exists?(:users, :confirmed_at)
+      t.datetime :confirmation_sent_at unless column_exists?(:users, :confirmation_sent_at)
+      t.string   :unconfirmed_email unless column_exists?(:users, :unconfirmed_email) # Only if using reconfirmable
+    end
   end
 end
